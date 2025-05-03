90aa2787baab5f046002e8467c2fe1a970bb2c26
Sophia Bean <75506473+sophiabean@users.noreply.github.com>
2025-05-03 13:55:09 -0400

    red positive auton changes (#3)

---
+++ .d/lemlib/chassis/odom.cpp.d
@@ -0,0 +1,50 @@
+bin/lemlib/chassis/odom.cpp.o: src/lemlib/chassis/odom.cpp \
+ include/pros/rtos.hpp include/pros/rtos.h include/pros/distance.hpp \
+ include/pros/device.hpp include/pros/misc.hpp include/pros/misc.h \
+ include/pros/distance.h include/lemlib/util.hpp \
+ include/lemlib/chassis/chassis.hpp include/pros/imu.hpp \
+ include/pros/imu.h include/lemlib/asset.hpp \
+ include/lemlib/chassis/trackingWheel.hpp include/pros/motors.hpp \
+ include/pros/abstract_motor.hpp include/pros/motors.h \
+ include/pros/rtos.hpp include/pros/motor_group.hpp \
+ include/pros/colors.hpp include/pros/adi.hpp include/pros/adi.h \
+ include/pros/rotation.hpp include/pros/rotation.h \
+ include/lemlib/pose.hpp include/lemlib/pid.hpp \
+ include/lemlib/exitcondition.hpp include/lemlib/driveCurve.hpp \
+ include/lemlib/timer.hpp include/lemlib/chassis/odom.hpp \
+ include/constants.hpp include/map.h include/particle_filter.h \
+ include/helper_functions.h include/map.h
+include/pros/rtos.hpp:
+include/pros/rtos.h:
+include/pros/distance.hpp:
+include/pros/device.hpp:
+include/pros/misc.hpp:
+include/pros/misc.h:
+include/pros/distance.h:
+include/lemlib/util.hpp:
+include/lemlib/chassis/chassis.hpp:
+include/pros/imu.hpp:
+include/pros/imu.h:
+include/lemlib/asset.hpp:
+include/lemlib/chassis/trackingWheel.hpp:
+include/pros/motors.hpp:
+include/pros/abstract_motor.hpp:
+include/pros/motors.h:
+include/pros/rtos.hpp:
+include/pros/motor_group.hpp:
+include/pros/colors.hpp:
+include/pros/adi.hpp:
+include/pros/adi.h:
+include/pros/rotation.hpp:
+include/pros/rotation.h:
+include/lemlib/pose.hpp:
+include/lemlib/pid.hpp:
+include/lemlib/exitcondition.hpp:
+include/lemlib/driveCurve.hpp:
+include/lemlib/timer.hpp:
+include/lemlib/chassis/odom.hpp:
+include/constants.hpp:
+include/map.h:
+include/particle_filter.h:
+include/helper_functions.h:
+include/map.h:
